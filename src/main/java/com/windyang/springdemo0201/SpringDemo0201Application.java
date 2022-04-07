823a625ce08775c38cd614dd35c1d0eb39cfd6b5
frankwin608 <frankwin608@gmail.com>
2022-04-07 16:58:09 +0800

    feat(SpringAction):springdemo init

---
+++ src/main/java/com/windyang/springdemo0201/SpringDemo0201Application.java
@@ -0,0 +1,16 @@
+package com.windyang.springdemo0201;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author windyang
+ */
+@SpringBootApplication
+public class SpringDemo0201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemo0201Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/windyang/springdemo0201/SpringDemo0201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.windyang.springdemo0201;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemo0201ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
