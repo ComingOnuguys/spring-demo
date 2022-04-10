7ce779a59a25ee2a260785a5c0dafdd7a33bec01
ComingOnuguys <frankwin608@gmail.com>
2022-04-11 02:35:30 +0800

    ConfigurationProperties

--- src/main/java/com/windyang/springdemo0201/controller/HomeController.java
+++ src/main/java/com/windyang/springdemo0201/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.windyang.springdemo0201.controller;
 
+
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/home")
 public class HomeController {
 
+
+    @Value("${taco.orders.pageSize}")
+    private int pageSize;
+
+
     @GetMapping("/index")
     public String index(){
+
+        System.out.println("pageSize = " + pageSize);
+
         return "index";
     }
 
